c7d5c2786e29cb48fc4f4ec1dcdcabfe5edf3c3d
yanbin <yanbin.shi@ly.com>
2023-06-19 20:08:39 +0800

    feat: 目录变更

--- autojs-web-control-docker/server/src/decorators/cur-user.ts
+++
@@ -1,7 +0,0 @@
-import { createParamDecorator } from "./../common/application";
-
-const CurUser = createParamDecorator((ctx) => {
-  return ctx.state.curUser;
-});
-
-export default CurUser;
---
+++ web-control/server/src/utils/db.ts
@@ -0,0 +1,23 @@
+import { DBM } from "../common/dbm";
+import getLogger from "./log4js";
+import { resolve } from "path";
+const ROOT = resolve(__dirname, "../../");
+require("dotenv").config({ path: `${ROOT}/.env.${process.env.USE_ENV}` });
+
+const logger = getLogger("db.ts");
+
+console.log("db:", process.env.MYSQL_HOST, process.env.MYSQL_PORT);
+
+const orm = new DBM({
+  connectionLimit: 10,
+  host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT),
+  user: "root",
+  password: "root",
+  database: "cloud_auto",
+  isDebug: true,
+});
+
+orm.setLogger(logger as any);
+
+export default orm;
